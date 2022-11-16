ec2d4a32cc65c8f4d3796403633a5464c6ba9e48
Miller Huang <miller.yc.huang@yangming.com>
2022-11-16 11:52:59 +0800

    Add button & select

--- ASP_Test/EndYear/EndYear/angular/src/app/app.module.ts
+++ ASP_Test/EndYear/EndYear/angular/src/app/app.module.ts
@@ -5,11 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     MenbersModule
--- ASP_Test/EndYear/EndYear/angular/src/app/menbers/upper/upper.component.ts
+++ ASP_Test/EndYear/EndYear/angular/src/app/menbers/upper/upper.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-upper',
   templateUrl: './upper.component.html',
   styleUrls: ['./upper.component.scss']
 })
 export class UpperComponent implements OnInit {
+  idx!: number;
+  heros = [
+    '董事長獎',
+    '總經理獎',
+    '資訊長獎',
+    '財務長獎',
+    '不讓你獎',
+    '虎年唬唬獎',
+    '偏偏要得獎'
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  // 內嵌繫結用變數
+  interpolation = '資訊部新人教育訓練';
+
+  // 事件繫結用變數
+  public onClickEvent() {
+    alert("You click me!");
   }
 
 }
